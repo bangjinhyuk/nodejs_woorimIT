a07b4105a6ec96da95e3ed83f6f31f55d4af4a43
bangjinhyuk <jinhyuk0103@gmail.com>
2021-02-28 19:52:30 +0900

    로그인화면 꾸미기

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -21,27 +21,6 @@ const process = {
         const response = user.login();
         return res.json(response)
 
-        // console.log("postlogin");
-
-        // const id = req.body.i d,
-        // psword = req.body.psword;
-        // console.log(id,psword);
-
-        // const users = UserStorage.getUsers("id","psword");
-        
-        // const response = {};
-        // if (users.id.includes(id)){
-        //     const idx = users.id.indexOf(id);
-        //     if (users.psword[idx] === psword){
-        //         response.success = true;
-        //         return res.json(response);
-        //     }
-        // }
-
-        // response.success = false;
-        // response.msg = "로그인에 실패하였습니다.";
-        // return res.json(response);
-
     },
 };
 
